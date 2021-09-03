11dafaec77530fad210fdc9ac16d81dfaf4096c4
Shaina Starr <starrs24@greenhill.org>
2021-09-03 13:00:50 -0500

    First Program

---
+++ hello.py
@@ -0,0 +1,28 @@
+# This if for comments
+
+# Shaina Starr
+# Date: 09/03/21
+# This is our first program. Test average
+
+#set variables
+test1=92
+test2=89
+test3=91
+test4=87
+
+#declare a average variable
+average=0
+print(type(average))
+#Four basic operators
+# Sum +
+# Sub -
+# Mult *
+# div /
+average= (test1+test2+test3+test4)/4
+# Let's print result
+print(type(average))
+#command using keywords
+print (average)
+print ("The test average is")
+print(average)
+
