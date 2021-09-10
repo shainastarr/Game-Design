ee7b119db0eb14fa5f2e4eb85aa20454b6d7309c
Shaina Starr <starrs24@greenhill.org>
2021-09-10 12:42:03 -0500

    looping

--- Looping.py
+++ Looping.py
@@ -1,21 +1,30 @@
 #Shaina Starr
 #09/08/2021
+import os
 
+os.system('cls')
 #We are going to learn how to ask the user for data
-#We are going to leanr how to loop
+#We are going to learn how to loop
 star= int(input("please enter number of stars ")) #allow to get values from the user
 print ("* * * *")
 #loop
 #variable to count lines
+sp=0
 line = star 
-for incounter in range(line):
-    for counter in range (star):
+for lncounter in range(line):
+    for space in range(lncounter):
+        print(" ", end=" ")
+    for counter in range(star):
         print ("*", end=" ") 
-    print ()
+    print (" ") 
 print ("Thank you")
 for incounter in range(line):
+    for space in range(lncounter):
+        print(" ", end=" ")
     for counter in range (star):
+        for space in range (spc)
         print ("*", end=" ") 
     print ()
     star -=1
 print ("Thank you")
+   
