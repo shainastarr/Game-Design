cce6f0f93558216718c1278130bb35234d2c9500
shainastarr <starrs24@greenhill.org>
2021-09-10 13:26:23 -0500

    Todays Workoki

---
+++ modulus.py
@@ -0,0 +1,20 @@
+#Shaina Starr
+#Learning about operators
+
+# + - / * add, sub, div, multi
+# ** exp
+# % mosulus reminder of a division 
+
+#Find an even number
+
+num=int(input("Give me a number"))
+print(num)
+if( num %2 == 0):
+    print(" the number is even")
+else:
+    print( "The number is odd")
+
+num2=float(input("give me any number no more than 2 decimals"))
+print(num2)
+print(num + num2)
+print(num2%2)
