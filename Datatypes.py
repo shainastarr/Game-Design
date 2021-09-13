44f454cf097138c832c1c71412ce98fa10dd7e9c
shainastarr <89479096+shainastarr@users.noreply.github.com>
2021-09-13 14:22:08 -0500

    Add files via upload
    added manually

---
+++ Datatypes.py
@@ -0,0 +1,25 @@
+
+int=4
+int2=8
+
+
+string1="shaina"
+string2="hello"
+
+boolean1=True
+boolean2=False
+
+float1=0.69
+float2=0.39
+
+print(int+int2)
+print(int-int2)
+print(int/int)
+print(int*int)
+print(float1+int2)
+print(float2*int2)
+
+print("boolean1 is a boolean2")
+print("float1 is a float")
+print("string1 is a string")
+print("int is a integer")
