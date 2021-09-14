9da2c804749c2fdffcce288d2dcf17e3ee2dfa8a
shainastarr <89479096+shainastarr@users.noreply.github.com>
2021-09-13 21:15:27 -0500

    Add files via upload

---
+++ Modulus 3.py
@@ -0,0 +1,21 @@
+#Shaina Starr
+#We are inputing the user to get the last digt, second to last digit, and then the third to last digit.
+
+
+import os
+os.system ('cls')
+num=int(input("give me a a number"))
+rem=num 
+#conditional
+if(rem==num):
+    print(rem%10)
+else:
+    print("fail")
+if(rem==num):
+    print("fail")
+
+if(rem==num):
+    print(rem%1000)
+
+else: 
+    print("fail")
