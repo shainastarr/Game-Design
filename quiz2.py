c829b2d44b24968dd3daf9e749f5f991bc72d557
shainastarr <89479096+shainastarr@users.noreply.github.com>
2021-10-04 12:50:10 -0500

    Add files via upload

---
+++ quiz2.py
@@ -0,0 +1,43 @@
+#This is quiz number 2
+
+#1. 
+print(len("Hello world!"))
+
+#2
+strString='Study.com is a wonderful resource.'
+print(strString.find('great'))
+print(strString.find('wonderful'))
+print(strString.find('.'))
+
+#3
+strString='Study.com is a wonderful resource'
+print(strString[15:24])
+
+#4
+'0', '1,' '2', '3', '4', '6', '7', '8', '9', '10'
+#the list starts at 0 because 0 is the first character in a string
+
+#5
+strString='Study.com is a wonderful resource.'
+print(strString[15:24].find('der')+len(strString))
+
+#6
+var1=17 
+var2=4
+result=var1 % var2
+print(result)
+
+#7
+print("\tContinue to start the game")
+
+#8
+print("Hello, my name is Shaina\n")
+
+#9
+user = "<your name>"
+print("Hello" + user + "!") 
+
+#10
+MyList=['0', '1,' '2', '3', '4', '6', '7', '8', '9', '10']
+MyList.insert(5,'5')
+print(MyList)
